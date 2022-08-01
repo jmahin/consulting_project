fde6cbb1a509d071c6af51e76ba9d6dbbee386c7
Mahin <78397547+jmahin@users.noreply.github.com>
2022-07-31 21:30:24 -0400

    Create index.php
    intro page

---
+++ index.php
@@ -0,0 +1,4 @@
+<h1>About</h1>
+<p>Hi! My name is Mahin and I'm from Boston, MA. I currently work in software quality assurance in the ed. tech industry. In 2022, I participated in the <a href="https://codesquad.org">CodeSquad</a> bootcamp, where I learnt HTML, CSS, Node.js, and MongoDB. I'm hoping to add React to my quiver in the near future. In the meanwhile, I am currently applying to bridge master's in computer science.</p>
+<h2>An unconventional path to web development</h2>
+<p>I graduated from <a href="https://umass.edu">UMass Amherst</a> in 2o18 with two degrees: biochemistry and environmental science. While I remain interested in my field of study, by reading journal articles and attending lectures, I haven't pursued a career <u>directly</u> related to either. 
